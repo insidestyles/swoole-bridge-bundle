57c35105be43b5411cafceb4d5d2ac4b090e5694
Michał Markiewicz <mmarkiewicz@gog.com>
2019-02-06 10:22:02 +0100

    extend base Symfony Command class

--- Command/SwooleBridgeServerCommand.php
+++ Command/SwooleBridgeServerCommand.php
@@ -3,6 +3,7 @@
 namespace Insidestyles\SwooleBridgeBundle\Command;
 
 use Insidestyles\SwooleBridge\Handler;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -10,7 +11,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  * Class SwooleBridgeServerCommand
  * @package Insidestyles\SwooleBridgeBundle\Command
  */
-final class SwooleBridgeServerCommand
+final class SwooleBridgeServerCommand extends Command
 {
     /**
      * @var Handler
@@ -29,6 +30,7 @@ final class SwooleBridgeServerCommand
 
     public function __construct(Handler $handler, string $host, int $port)
     {
+        parent::__construct();
         $this->handler = $handler;
         $this->host    = $host;
         $this->port    = $port;
